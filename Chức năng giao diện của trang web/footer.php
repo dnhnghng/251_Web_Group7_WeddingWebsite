bbfd8204c028551ce208c34a778b44c1802abc83
Tester-ben <phantu210206@gmail.com>
2025-11-21 09:03:42 +0700

    Delete Chức năng giao diện của trang web directory

--- Chức năng giao diện của trang web/footer.php
+++
@@ -1,9 +0,0 @@
-</main> <footer>
-        <div class="container">
-            <p>&copy; 2025 WeddingBliss. Phát triển bởi <?php echo $_SESSION['customer_name'] ?? 'Nhóm 7'; ?>.</p>
-            <p>Đồ án môn học Lập trình Ứng dụng web.</p>
-        </div>
-    </footer>
-
-</body>
-</html>
